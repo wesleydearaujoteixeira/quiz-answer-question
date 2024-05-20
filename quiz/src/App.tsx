082c65451297aae107847ce4f6d05619d3a23f8d
wesley <wesleyaraujoteixeira98@gmail.com>
2024-05-20 19:49:43 -0300

    initializing the project

---
+++ quiz/src/App.tsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='main'>
+        <section>
+          <ul className='ul-container'>
+            <div>
+              <li>a</li>
+              <li>b</li>
+            </div>
+            <div>
+              <li>c</li>
+              <li>d</li>
+            </div>
+          </ul>
+        </section>
+      </div>
+    </>
+  )
+}
+
+export default App
