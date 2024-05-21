fd4ec5eabff618cdf12cd04fb3997ff57f56d4ed
wesley <wesleyaraujoteixeira98@gmail.com>
2024-05-20 22:55:02 -0300

    update of the project

--- quiz/src/App.tsx
+++ quiz/src/App.tsx
@@ -1,22 +1,42 @@
 
+import { useState } from 'react'
 import './App.css'
+import { questios_answers} from './questions/quetions'
 
 function App() {
+
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+
+  const theNextQuestion = () => {
+
+    if(questios_answers.length > currentIndex && currentIndex === questios_answers.length - 2){
+      setCurrentIndex(0);
+    }
+    else {
+      setCurrentIndex(currentIndex + 1);
+    }
+
+    console.log('length: ', questios_answers.length);
+    console.log('currentIndex: ', currentIndex);
+  
+  
+  }; 
 
   return (
     <>
       <div className='main'>
-        <section>
-          <ul className='ul-container'>
-            <div>
-              <li>a</li>
-              <li>b</li>
-            </div>
-            <div>
-              <li>c</li>
-              <li>d</li>
-            </div>
-          </ul>
+
+      <h1>{questios_answers[currentIndex].title}</h1>
+
+        <section className='container'>
+          {questios_answers[currentIndex].questions.map((item) => {
+            return (
+                <h1> {item} </h1>
+            );
+          })}
+          <div><button onClick={() => theNextQuestion()}> Próximo </button></div>
         </section>
       </div>
     </>
---
+++ quiz/src/questions/quetions.ts
@@ -0,0 +1,133 @@
+export type QuestionAnswers = {
+    id: number, 
+    title: string, 
+    questions: string[],
+    currect: number
+}
+
+
+export const questios_answers:  QuestionAnswers[] = [
+  
+
+            {
+                id: 1, 
+                title: 'Qual o Paí inventou o Futebol?',
+
+                questions: [
+                    ' a) Brasil',
+                    ' b) Argentina',
+                    ' c) Inglaterra',
+                    ' d) Japão', 
+                ],
+
+                currect: 0
+            },
+
+            {
+                id: 2, 
+                title: 'Quem descobriu a teoria da Relatividade',
+                questions: [
+                     'a) Isaac Newton',
+                     'b) Sócrates',
+                     'c) Bleise Pascal',
+                     'd) Albert Einstein', 
+                ],
+
+                currect: 3
+            },
+
+            {
+                id: 4, 
+                title: 'Qual número é usado para calcular a área da circunferência?',
+                questions: [
+                     'a) 2,72',
+                     'b) 17,28',
+                     'c) 3,14',
+                     'd) 1,34', 
+                ],
+
+                currect: 2
+            },
+            {
+                id: 4, 
+                title: 'Qual número é usado para calcular a área da circunferência?',
+                questions: [
+                     'a) 2,72',
+                     'b) 17,28',
+                     'c) 3,14',
+                     'd) 1,34', 
+                ],
+
+                currect: 2
+            }, {
+                id: 4, 
+                title: 'Qual número é usado para calcular a área da circunferência?',
+                questions: [
+                     'a) 2,72',
+                     'b) 17,28',
+                     'c) 3,14',
+                     'd) 1,34', 
+                ],
+
+                currect: 2
+            }, 
+            
+             {
+                id: 4, 
+                title: 'Qual número é usado para calcular axxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx área da circunferência?',
+                questions: [
+                     'a) 2,72',
+                     'b) 17,28',
+                     'c) 3,14',
+                     'd) 1,34', 
+                ],
+
+                currect: 2
+            }, {
+                id: 4, 
+                title: 'Qual número é usado para calcul54335464575687568642345436ar a área da circunferência?',
+                questions: [
+                     'a) 2,72',
+                     'b) 17,28',
+                     'c) 3,14',
+                     'd) 1,34', 
+                ],
+
+                currect: 2
+            }, {
+                id: 4, 
+                title: 'Qual número é usado para calc34345r24313123123123ular a área da circunferência?',
+                questions: [
+                     'a) 2,72',
+                     'b) 17,28',
+                     'c) 3,14',
+                     'd) 1,34', 
+                ],
+
+                currect: 2
+            }, {
+                id: 4, 
+                title: 'Qual número é usadfggrtr554444444 para calcular a área da circunferência?',
+                questions: [
+                     'a) 2,72',
+                     'b) 17,28',
+                     'c) 3,14',
+                     'd) 1,34', 
+                ],
+
+                currect: 2
+            }, {
+                id: 4, 
+                title: 'Qual número é dsadasdasdasusado para calcular a área da circunferência?',
+                questions: [
+                     'a) 2,72',
+                     'b) 17,28',
+                     'c) 3,14',
+                     'd) 1,34', 
+                ],
+
+                currect: 2
+            },
+         
+    
+]
